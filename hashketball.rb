3efc0d04e5a55bddb39d72f5c2e0d91ed75cbf56
Andrew Cohn <andrewcohn0@gmail.com>
2018-02-23 11:30:53 -0500

    update solution to reflect readme instructions

--- hashketball.rb
+++ hashketball.rb
@@ -1,11 +1,12 @@
+require 'pry'
+
 def game_hash
   {
     home: {
       team_name: "Brooklyn Nets",
       colors: ["Black", "White"],
-      players: [
-        {
-          player_name: "Alan Anderson",
+      players: {
+        "Alan Anderson" => {
           number: 0,
           shoe: 16,
           points: 22,
@@ -14,8 +15,8 @@ def game_hash
           steals: 3,
           blocks: 1,
           slam_dunks: 1
-        }, {
-          player_name: "Reggie Evans",
+        },
+        "Reggie Evans" => {
           number: 30,
           shoe: 14,
           points: 12,
@@ -24,8 +25,8 @@ def game_hash
           steals: 12,
           blocks: 12,
           slam_dunks: 7
-        }, {
-          player_name: "Brook Lopez",
+        },
+        "Brook Lopez" => {
           number: 11,
           shoe: 17,
           points: 17,
@@ -34,8 +35,8 @@ def game_hash
           steals: 3,
           blocks: 1,
           slam_dunks: 15
-        }, {
-          player_name: "Mason Plumlee",
+        },
+        "Mason Plumlee" => {
           number: 1,
           shoe: 19,
           points: 26,
@@ -44,8 +45,8 @@ def game_hash
           steals: 3,
           blocks: 8,
           slam_dunks: 5
-        }, {
-          player_name: "Jason Terry",
+        },
+        "Jason Terry" => {
           number: 31,
           shoe: 15,
           points: 19,
@@ -55,14 +56,13 @@ def game_hash
           blocks: 11,
           slam_dunks: 1
         }
-      ]
+      }
     },
     away: {
       team_name: "Charlotte Hornets",
       colors: ["Turquoise", "Purple"],
-      players: [
-        {
-          player_name: "Jeff Adrien",
+      players: {
+        "Jeff Adrien" => {
           number: 4,
           shoe: 18,
           points: 10,
@@ -71,8 +71,8 @@ def game_hash
           steals: 2,
           blocks: 7,
           slam_dunks: 2
-        }, {
-          player_name: "Bismak Biyombo",
+        },
+        "Bismak Biyombo" => {
           number: 0,
           shoe: 16,
           points: 12,
@@ -81,8 +81,8 @@ def game_hash
           steals: 7,
           blocks: 15,
           slam_dunks: 10
-        }, {
-          player_name: "DeSagna Diop",
+        },
+        "DeSagna Diop" => {
           number: 2,
           shoe: 14,
           points: 24,
@@ -91,8 +91,8 @@ def game_hash
           steals: 4,
           blocks: 5,
           slam_dunks: 5
-        }, {
-          player_name: "Ben Gordon",
+        },
+        "Ben Gordon" => {
           number: 8,
           shoe: 15,
           points: 33,
@@ -101,8 +101,8 @@ def game_hash
           steals: 1,
           blocks: 1,
           slam_dunks: 0
-        }, {
-          player_name: "Brendan Haywood",
+        },
+        "Brendan Haywood" => {
           number: 33,
           shoe: 15,
           points: 6,
@@ -112,14 +112,9 @@ def game_hash
           blocks: 5,
           slam_dunks: 12
         }
-      ]
+      }
     }
   }
-end
-
-def shoe_size(name)
-  player = find_the_player(name)
-  player.fetch(:shoe)
 end
 
 def num_points_scored(name)
@@ -127,50 +122,50 @@ def num_points_scored(name)
   player.fetch(:points)
 end
 
+def shoe_size(name)
+  player = find_the_player(name)
+  player.fetch(:shoe)
+end
+
 def team_colors(team_name)
   team = find_the_team(team_name)
   team.fetch(:colors)
+end
+
+def team_names
+  teams.map{|t| t.fetch(:team_name)}
+end
+
+def player_numbers(team_name)
+  find_the_team(team_name)[:players].map{ |player_name, stats| stats[:number] }
+end
+
+def player_stats(player_name)
+  find_the_player(player_name)
+end
+
+def big_shoe_rebounds
+  player_biggest_shoe_size.fetch(:rebounds)
 end
 
 def teams
   game_hash.values
 end
 
+def players
+  game_hash[:home][:players].merge(game_hash[:away][:players])
+end
+
 def find_the_team(team_name)
   teams.find {|team| team.fetch(:team_name) == team_name}
 end
 
-def big_shoe_rebounds
-  player = player_biggest_shoe_size
-  player.fetch(:rebounds)
+def find_the_player(name)
+  game_hash[:home][:players][name] ?
+  game_hash[:home][:players][name]
+  : game_hash[:away][:players][name]
 end
 
 def player_biggest_shoe_size
-  players.sort_by {|player| player.fetch(:shoe) }.last
-end
-
-def players
-  home_players = game_hash.fetch(:home).fetch(:players)
-  away_players = game_hash.fetch(:away).fetch(:players)
-  home_players + away_players
-end
-
-def team_names
-  teams.map do |team|
-    team[:team_name]
-  end
-end
-
-def player_numbers(team_name)
-  find_the_team(team_name)[:players].map do |player|
-    player[:number]
-  end
-end
-
-def player_stats(player_name)
-  find_the_player(player_name).reject { |key, value| key == :player_name }
-end
-
-def find_the_player(name)
-  players.find {|player| player.fetch(:player_name) == name}
+  players.max_by{|player, stats| stats.fetch(:shoe)}[1]
 end
