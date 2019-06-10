b656114c82049ed15f34f048c9e5cf6c393e45be
maxwellbenton <maxwellbenton@gmail.com>
2019-06-10 12:42:53 -0400

    updates solution stat

--- hashketball.rb
+++ hashketball.rb
@@ -86,7 +86,7 @@ def game_hash
                 number: 1,
                 shoe: 19,
                 points: 26,
-                rebounds: 12,
+                rebounds: 11,
                 assists: 6,
                 steals: 3,
                 blocks: 8,
